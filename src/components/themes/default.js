87f10cd503580e0097eab4ee899e1a4f8253d40e
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 14:00:11 +0200

    fix: colors

--- src/components/themes/default.js
+++ src/components/themes/default.js
@@ -3,22 +3,20 @@ import { reversePalette } from 'styled-theme/composer'
 const theme = {}
 
 theme.palette = {
-  primary: ['#1976d2', '#2196f3', '#71bcf7', '#c2e2fb'],
-  secondary: ['#c2185b', '#e91e63', '#f06292', '#f8bbd0'],
+  primary: ['#6A9AE2', '#E7F0FF'],
+  secondary: ['#000', '#000'],
   danger: ['#d32f2f', '#f44336', '#f8877f', '#ffcdd2'],
   alert: ['#ffa000', '#ffc107', '#ffd761', '#ffecb3'],
   success: ['#388e3c', '#4caf50', '#7cc47f', '#c8e6c9'],
-  white: ['#fff', '#fff', '#eee'],
-  grayscale: [
-    '#212121',
-    '#414141',
-    '#616161',
-    '#9e9e9e',
-    '#bdbdbd',
-    '#e0e0e0',
-    '#eeeeee',
-    '#ffffff',
-  ],
+  white: ['#f6f6f6', '#fcfcfc', '#fff'],
+  black: ['#444', '#222', '#000'],
+  background: '#eee',
+  grayScale: ['#444', '#888', '#BBB', '#eee', '#F6F6F6', '#FCFCFC'],
+}
+
+theme.shadows = {
+  dropShadow: '0 3px 4px rgba(0,0,0, 0.5)',
+  innerShadow: '0 3px 4px rgba(0,0,0, 0.5)',
 }
 
 theme.reversePalette = reversePalette(theme.palette)
