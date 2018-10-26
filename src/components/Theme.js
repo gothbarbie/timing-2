b6763c922be784f7760359e690db1b014f5d5722
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:49:55 +0200

    chore: add proptypes

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { ThemeProvider } from 'styled-components'
 
 import theme from './themes/default'
@@ -6,5 +7,9 @@ import theme from './themes/default'
 const Theme = ({ children }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
 )
+
+Theme.propTypes = {
+  children: PropTypes.node,
+}
 
 export default Theme
