e1e22327bb57ca3b260bf0e74ffbe4a9ba0f293d
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 14:39:47 +0200

    test: fix test, add props

--- src/components/atoms/IconButton/__tests__/IconButton.spec.js
+++ src/components/atoms/IconButton/__tests__/IconButton.spec.js
@@ -5,7 +5,14 @@ import IconButton from '../IconButton'
 
 describe('components/atoms/IconButton', () => {
   it('renders', () => {
-    const component = shallow(<IconButton />)
+    const props = {
+      color: 'gray',
+      hoverColor: 'gray',
+      icon: ['fas', 'chevron-down'],
+      size: 'lg',
+      onClick: jest.fn(),
+    }
+    const component = shallow(<IconButton {...props} />)
 
     expect(component).toMatchSnapshot()
   })
