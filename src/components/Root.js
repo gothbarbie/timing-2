aaf37b3cef4ba8914d40c365a649f102482bb36f
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:49:47 +0200

    chore: add proptypes

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 
 import store from '../store/configureStore'
 
 const Root = ({ children }) => <Provider store={store}>{children}</Provider>
+
+Root.propTypes = {
+  children: PropTypes.node,
+}
 
 export default Root
