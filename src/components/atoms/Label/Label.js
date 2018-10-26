e705d5d346995705b9d605ae587c280668bca1ab
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 14:40:16 +0200

    style: fix color

--- src/components/atoms/Label/Label.js
+++ src/components/atoms/Label/Label.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Label = styled.label`
   font-size: 1.4rem;
   font-weight: 500;
-  color: ${({ theme }) => theme.palette.grayScale[0]};
+  color: ${({ theme }) => theme.palette.grayScale[1]};
 `
 
 export default Label
