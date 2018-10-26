7ce165724cec739954792adc022047b8b448df23
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 14:42:34 +0200

    chore: add tags

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,11 +6,13 @@ import categoryReducer from '../components/molecules/Category/categoryReducer'
 import categoriesFilterReducer from '../components/molecules/SearchCategories/categoriesFilterReducer'
 import modalReducer from '../components/molecules/Modal/modalReducer'
 import weekReducer from '../components/organisms/WeekNavigation/weekReducer'
+import tagsReducer from '../components/atoms/Tag/tagsReducer'
 
 const rootReducer = combineReducers({
   categories: categoryReducer,
   categoriesFilter: categoriesFilterReducer,
   modal: modalReducer,
+  tags: tagsReducer,
   week: weekReducer,
 })
 
