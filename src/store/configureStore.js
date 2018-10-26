830e84b31b0013277c1d8f6451e176a197fcea5e
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 14:00:25 +0200

    fix: add missing reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,17 @@ import { combineReducers, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const basicReducer = (state = {}, action) => {
-  return state
-}
+import categoryReducer from '../components/molecules/Category/categoryReducer'
+import categoriesFilterReducer from '../components/molecules/SearchCategories/categoriesFilterReducer'
+import modalReducer from '../components/molecules/Modal/modalReducer'
+import weekReducer from '../components/organisms/WeekNavigation/weekReducer'
 
-const rootReducer = combineReducers({ default: basicReducer })
+const rootReducer = combineReducers({
+  categories: categoryReducer,
+  categoriesFilter: categoriesFilterReducer,
+  modal: modalReducer,
+  week: weekReducer,
+})
 
 const store = createStore(
   rootReducer,
