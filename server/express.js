90724e0458279edfd467e827fe23bec800d0cd51
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:48:56 +0200

    chore: add local server

---
+++ server/express.js
@@ -0,0 +1,34 @@
+const express = require('express')
+// const path = require('path')
+
+// Define server
+const server = express()
+
+const isProd = process.env.NODE_ENV === 'production'
+if (!isProd) {
+  // WebpackDevMiddleware
+  const webpack = require('webpack')
+  const config = require('../webpack.dev.js')
+  const compiler = webpack(config)
+
+  const webpackDevMiddleware = require('webpack-dev-middleware')(
+    compiler,
+    config.devServer
+  )
+  server.use(webpackDevMiddleware)
+
+  // WebpackHotMiddleware
+  const webpackHotMiddleware = require('webpack-hot-middleware')(compiler)
+  server.use(webpackHotMiddleware)
+}
+
+// StaticMiddleware
+// const staticMiddleware = express.static('dist')
+// server.use(staticMiddleware)
+const expressStaticGzip = require('express-static-gzip')
+server.use(expressStaticGzip('dist', { enableBrotli: true }))
+
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`)
+})
