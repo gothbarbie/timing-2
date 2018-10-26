a68c2926e677d33ffcacb702c694f20083b9b62d
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:52:03 +0200

    fix: color refs and add proptypes

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,21 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const InputStyle = styled.input`
   border-radius: 4px;
-  border: 1px solid ${({ theme }) => theme.colors.gray};
+  border: 1px solid ${({ theme }) => theme.palette.grayScale[0]};
   padding: 1rem 1.5rem;
   font-size: 1.6rem;
-  color: ${({ theme }) => theme.colors.dark};
+  color: ${({ theme }) => theme.palette.grayScale[0]};
   outline: none;
 
   &:focus {
-    border: 1px solid ${({ theme }) => theme.colors.primary};
+    border: 1px solid ${({ theme }) => theme.palette.primary[0]};
   }
 `
 
 const Input = ({ name, onChange, type = 'text' }) => {
   return <InputStyle name={name} onChange={onChange} type={type} />
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string,
 }
 
 export default Input
