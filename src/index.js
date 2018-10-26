c7fe81d24745a35e3adf1cf6081948901ab966f2
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:49:15 +0200

    fix: add missing icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faArrowCircleLeft,
+  faArrowCircleRight,
+  faCog,
+  faEllipsisH,
+  faPencilAlt,
+  faPlusCircle,
+  faSearch,
+  faTimes,
+  faTimesCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
 import App from './components/App'
+
+library.add(
+  faArrowCircleLeft,
+  faArrowCircleRight,
+  faCog,
+  faEllipsisH,
+  faPencilAlt,
+  faPlusCircle,
+  faSearch,
+  faTimes,
+  faTimesCircle
+)
 
 render(<App />, document.getElementById('root'))
