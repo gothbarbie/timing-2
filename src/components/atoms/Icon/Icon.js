55371959f8a6917765f8b85d8c9f286f211f5e5a
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:51:11 +0200

    fix: color refs and add proptypes

--- src/components/atoms/Icon/Icon.js
+++ src/components/atoms/Icon/Icon.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 // NOTE! All icons used must be manually added at src/index.js
 
 const IconStyle = styled(FontAwesomeIcon)`
-  color: ${({ color, theme }) => color === 'white' && theme.colors.white};
-  color: ${({ color, theme }) => color === 'gray' && theme.colors.gray};
+  color: ${({ color, theme }) => color === 'white' && theme.palette.white[0]};
+  color: ${({ color, theme }) =>
+    color === 'gray' && theme.palette.grayScale[0]};
 `
 
 const Icon = ({ color, icon, size }) => (
   <IconStyle color={color} icon={icon} size={size} />
 )
+
+Icon.propTypes = {
+  color: PropTypes.string,
+  icon: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  size: PropTypes.string,
+}
 
 export default Icon
