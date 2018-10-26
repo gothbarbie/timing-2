1b1283ff317804822f7bde3c7aa3f65c59711705
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-26 13:52:18 +0200

    fix: color refs

--- src/components/atoms/Label/Label.js
+++ src/components/atoms/Label/Label.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Label = styled.label`
   font-size: 1.4rem;
   font-weight: 500;
-  color: ${({ theme }) => theme.colors.dark};
+  color: ${({ theme }) => theme.palette.grayScale[0]};
 `
 
 export default Label
