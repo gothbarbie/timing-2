16d6ffa3a36fbdc6548ed57b15111dce4a6bf6c5
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-28 13:51:31 +0100

    chore: add tagsActions

--- src/actions/tagsActions.js
+++ src/actions/tagsActions.js
@@ -1,12 +1,41 @@
+import db from '../firebase/firebase'
+
+import { databaseError } from './databaseErrorActions'
+
 export const ADD_TAG = 'ADD_TAG'
 export const REMOVE_TAG = 'REMOVE_TAG'
+export const CREATE_TAG_SUCCESS = 'CREATE_TAG_SUCCESS'
+export const CREATE_TAG_FAIL = 'CREATE_TAG_FAIL'
 
 export const addTag = ({ id, blue, name }) => ({
   type: ADD_TAG,
   payload: { id, blue, name },
 })
 
-export const removeTag = id => ({
+export const remove = id => ({
   type: REMOVE_TAG,
   payload: id,
 })
+
+const createTagSuccess = ({ id, blue, name }) => ({
+  type: CREATE_TAG_SUCCESS,
+  payload: { id, blue, name },
+})
+
+const createTagFail = ({ id, blue, name }) => ({
+  type: CREATE_TAG_FAIL,
+  payload: { id, blue, name },
+})
+
+export const createTagInDatabase = ({ id, blue, name }) => dispatch => {
+  const tag = { blue, name }
+  db.ref(`tag/${id}`)
+    .set(tag)
+    .then(() => {
+      dispatch(createTagSuccess({ id, blue, name }))
+    })
+    .catch(error => {
+      dispatch(createTagFail({ id, blue, name }))
+      dispatch(databaseError(error))
+    })
+}
